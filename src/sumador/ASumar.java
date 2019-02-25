ad671469dd0909a9d2eaf2eca3bb5de43fbfe272
Gabriel <Gabriel@192.168.1.105>
2019-02-25 17:37:24 +0100

    Primera Prueba Completada

---
+++ src/sumador/ASumar.java
@@ -0,0 +1,30 @@
+package sumador;
+
+public class ASumar {
+
+	/**
+	 * Variable que guarda el texto con los números que se van a sumar.
+	 */
+	private String valor_inicial;
+
+	/**
+	 * Método contructor de la clase.
+	 * @param string : Número, o números que se van a sumar.
+	 */
+	public ASumar(String string) {
+        this.valor_inicial = string;
+    }
+
+	/**
+	 * Función que devuelve como String el número de la suma de los números que forman el valor_inicial.
+	 * @return Número obtenido como resultado de la suma de los números que forman el valor_inicial.
+	 */
+	public String mostrar() {
+        String numero = valor_inicial;
+        if (numero.length() == 1) {
+            return (numero + " = " + numero);
+        }
+        return numero;
+	}
+
+}
---
+++ test/sumador/TestASumar.java
@@ -0,0 +1,16 @@
+package sumador;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestASumar {
+
+	@Test
+    public void testUnaCifra() {
+        ASumar sumi = new ASumar("5");
+        String resultado = sumi.mostrar();
+        assertEquals("5 = 5",resultado );
+    }
+
+}
